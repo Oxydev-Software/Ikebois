4b358587cb2d78f944760a897e5afd59f88625e7
daynox <doureyd@gmail.com>
2017-12-13 10:53:00 +0100

    Working Product Page

--- App.js
+++ App.js
@@ -1,38 +1,10 @@
 import React from 'react';
 import { StyleSheet, Text, View, TextInput } from 'react-native';
 import { Button } from 'react-native-elements';
+import ProductPage from './Products';
 
 export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { text: 'Useless Placeholder' };
-  }
   render() {
-    return (
-      <View style={{ flex: 1 }}>
-        <View style={{ flex: 1, backgroundColor: 'powderblue' }} />
-        <View style={{ flex: 1, backgroundColor: 'steelblue' }}>
-          <TextInput
-            style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
-            onChangeText={text => this.setState({ text })}
-            value={this.state.text}
-          />
-        </View>
-        <View style={{ flex: 4, backgroundColor: 'skyblue' }} />
-      </View>
-    );
+    return <ProductPage />;
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#2767AC',
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
-  text: {
-    color: '#fff',
-    fontSize: 18
-  }
-});
